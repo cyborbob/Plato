39157a3914919e351444d455f1f2bf1b1fcb1ffe
cyborbob <rdf_errorzone@hotmail.com>
2013-11-29 21:02:19 +0100

    Added Test Project.

---
+++ PlatoReasoningEngine/EngineTest/CommandInterpreterTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EngineTest
+{
+    [TestClass]
+    public class CommandInterpreterTest
+    {
+
+
+
+        [TestMethod]
+        public void Templete_StateUnderTest_ExpectedOutcome()
+        {    
+            // Arrange.
+            
+            // Act.
+            
+            // Assert.
+                
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
